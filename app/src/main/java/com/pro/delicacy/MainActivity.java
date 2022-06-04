81289cf7a716ff37c95be6704c251e688231c15c
Okellothomas <thomasolal33@gmail.com>
2022-06-05 01:13:42 +0300

    add the api key implementation

--- app/src/main/java/com/pro/delicacy/MainActivity.java
+++ app/src/main/java/com/pro/delicacy/MainActivity.java
@@ -2,7 +2,9 @@ package com.pro.delicacy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // add the splash functionality.
+
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Delicacies.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1000);
     }
 }
---
+++ app/src/main/java/com/pro/delicacy/models/RandomResponse.java
@@ -0,0 +1,38 @@
+package com.pro.delicacy.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class RandomResponse {
+
+    @SerializedName("meals")
+    @Expose
+    private List<Meal> meals = null;
+
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public RandomResponse() {
+    }
+
+    /**
+     * 
+     * @param meals
+     */
+    public RandomResponse(List<Meal> meals) {
+        super();
+        this.meals = meals;
+    }
+
+    public List<Meal> getMeals() {
+        return meals;
+    }
+
+    public void setMeals(List<Meal> meals) {
+        this.meals = meals;
+    }
+
+}
