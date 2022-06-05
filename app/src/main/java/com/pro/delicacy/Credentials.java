1ee2f8b34af5f993eb2e38c606d0446a04505cde
Okellothomas <thomasolal33@gmail.com>
2022-06-05 07:38:07 +0300

    add the internet permission

---
+++ app/src/main/java/com/pro/delicacy/Credentials.java
@@ -0,0 +1,7 @@
+package com.pro.delicacy;
+
+public class Credentials {
+
+        public static final String BASE_URL = "www.themealdb.com/api/json/v1/1/";
+        public static final int API_KEY = BuildConfig.DELICACIES_API_KEY;
+}
