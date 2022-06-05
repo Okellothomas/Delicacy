43c0ad3cb8a40ddae2ab9e96bcc824db0db89189
Okellothomas <thomasolal33@gmail.com>
2022-06-05 11:35:58 +0300

    add the catogories adapter and its viewholder class

---
+++ app/src/main/java/com/pro/delicacy/adapters/CategoriesAdapter.java
@@ -0,0 +1,16 @@
+package com.pro.delicacy.adapters;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CategoriesAdapter {
+
+    public class myViewHolder extends RecyclerView.ViewHolder{
+
+        public myViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
